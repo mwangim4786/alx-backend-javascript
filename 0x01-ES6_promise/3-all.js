de709df66a90e65ae8acbcb45f608abc6420bad9
Martin Kihungi <mwangim4786@gmail.com>
2024-08-07 22:20:15 +0000

    3-all.js 1

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promises = [uploadPhoto(), createUser()]
-  return Promise.all(promises).then((values) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
     console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
   })
 }
