048db10c0fe570a8e1c8f728c2cd879365e1f1b6
Martin Kihungi <mwangim4786@gmail.com>
2024-08-07 21:23:31 +0000

    1-promise.js 5

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,10 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => { 
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(Error('The fake API is not working currently'));
-    }
-  });
+  if (success) {
+    return Promise.resolve({
+      status: 200,
+      body: 'Success',
+    });
+  } else {
+    return Promise.reject(Error('The fake API is not working currently'));
+  }
 }
