338f8d847da167b813be63adcd80aa3b26dc486c
Martin Kihungi <mwangim4786@gmail.com>
2024-08-08 01:41:57 +0300

    1-promise.js 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,6 +3,6 @@ export default function handleResponseFromAPI(promise) {
     status: 200,
     body: 'success',
   }))
-    .catch(() => Error());
+    .catch(() => Error())
     .finally(() => console.log('Got a response from the API'));
 }
